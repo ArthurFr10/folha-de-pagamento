cc8b5a1d0ecd1089ead6fcb40f17e2eaf2bcfb38
ArthurFr10 <arthurfragabbmp@gmail.com>
2024-10-24 01:45:52 +0000

    salvar

--- folha-de-pagamento.py
+++ folha-de-pagamento.py
@@ -33,46 +33,61 @@ def lendo_matricula():
             limpa_tela()
             print("Tente novamente")
             
-
-def perguntando(a):
-    a.salario_bruto = float(input("Digite o seu salário bruto: "))
-    a.dependentes = int(input)
-    a.vale_transporte = input("Deseja receber o vale transporte(sim / não)?")
-    a.vale_refeicao = float(input("Digite o valor do vale refeição: "))
-    lista_funcionario.append(a)
+def perguntando():
+    salario_bruto = float(input("Digite o seu salário bruto: "))
+    dependentes = int(input("Digite quantos dependentes você tem: "))
+    vale_transporte = bool(input("Deseja receber o vale transporte(sim / não)?"))
+    vale_refeicao = float(input("Digite o valor do vale refeição: "))
+    return salario_bruto, dependentes, vale_transporte, vale_refeicao
+    
 
 def INSS(salario):
     if salario <= 1100.00:
         desconto = salario * 0.075
-        salario_liquido = salario - desconto
-        return salario_liquido
+        return desconto
     elif salario >= 1100.01 and salario <= 2203.48:
         desconto = salario * 0.09
-        salario_liquido = salario - desconto
-        return salario_liquido
+        return desconto
     elif salario >= 2203.49 and salario <= 3305.22:
         desconto = salario * 0.12
-        salario_liquido = salario - desconto
-        return salario_liquido
+        return desconto
     elif salario >= 3305.23 and salario <= 6433.57:
         desconto = salario * 0.14
-        salario_liquido = salario - desconto
-        return salario_liquido
+        return desconto
     else:
-        salario_liquido = salario - 854.36
-        return salario_liquido
+        desconto = salario - 854.36
+        return desconto
 
-def IRRF(salario):
+def IRRF(salario, depedente):
     if salario >= 2259.21 and salario <= 2826.65:
         desconto = salario * 0.075
-        salario_liquido = salario - desconto
-        return salario_liquido
+        return desconto
     elif salario >= 2826.66 and salario <= 3751.05:
         desconto = salario * 0.15
-        salario_liquido = salario - desconto
-        return salario_liquido
+        return desconto
     elif salario >= 3751.06 and salario <= 4664.68:
-        desconto = salario * 0. 
+        desconto = salario * 0.225
+        return desconto
+    elif salario >= 4664.68:
+        desconto = salario * 0.275
+        return desconto
+    deducao_por_depedente = 189.59
+    deducao_total = depedente - deducao_por_depedente
+    return deducao_total
+
+def vale_transportes(salario, variavel_transporte):
+    if variavel_transporte == "sim":
+        desconto = salario * 0.06
+        return desconto
+    return 0
+
+def vale_refeicoes(salario, variavel_refeicao):
+    desconto = variavel_refeicao * 0.20
+    return desconto
+
+def plano_de_saude(salario, dependente):
+    desconto = dependente * 150.00
+    return desconto
 limpa_tela()
 while True:
     opcao = int(input("""Seja Bem Vindo ao sistema. O que deseja fazer?
@@ -85,10 +100,15 @@ while True:
             
         case 2:
             funcionario = lendo_matricula()
-            perguntando(funcionario)
+            salario_bruto, dependentes, vale_transporte, vale_refeicao = perguntando()
             break
-while True:
-
+desconto_inss = INSS(salario_bruto)
+desconto_irff = IRRF(salario_bruto, dependentes)
+desconto_vale_transporte = vale_transportes(salario_bruto, vale_transporte)
+desconto_vale_refeicao = vale_refeicoes(salario_bruto, vale_refeicao)
+desconto_plano_de_saude = plano_de_saude(salario_bruto, dependentes)
+salario_liquido = salario_bruto - (desconto_inss + desconto_irff + desconto_vale_transporte + desconto_vale_refeicao + desconto_plano_de_saude)
+print(f"{funcionario.nome} tem de salário líquido: R$ {salario_liquido:.2f}")
 
 
                 
